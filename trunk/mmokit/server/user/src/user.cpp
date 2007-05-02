57ebd14b3777232cae8f3d98e9b42efaa2808c87
JeffM2501 <JeffM2501@b8188ba6-572f-0410-9fd0-1f3cfbbf368d>
2007-05-02 21:52:48 +0000

    userDB

---
+++ trunk/mmokit/server/user/src/user.cpp
@@ -0,0 +1,15 @@
+// user.cpp : Defines the entry point for the console application.
+//
+
+#include "database.h"
+#include "tcpConnection.h"
+
+
+int main(int argc, char* argv[])
+{
+	Database	database("characters.dbf");
+
+
+	return 0;
+}
+
