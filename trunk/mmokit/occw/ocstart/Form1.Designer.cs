b17eed5f1758a934cf6edb79844765856da48ca8
JeffM2501 <JeffM2501@b8188ba6-572f-0410-9fd0-1f3cfbbf368d>
2009-01-26 16:31:06 +0000

    don't use a thread, use a button to patch, until we know the patcher works.

--- trunk/mmokit/occw/ocstart/Form1.Designer.cs
+++ trunk/mmokit/occw/ocstart/Form1.Designer.cs
@@ -43,6 +43,7 @@ namespace ocstart
             this.SaveCred = new System.Windows.Forms.CheckBox();
             this.LoginGroup = new System.Windows.Forms.GroupBox();
             this.FullCheck = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.LoginGroup.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -195,6 +196,16 @@ namespace ocstart
             this.FullCheck.UseVisualStyleBackColor = true;
             this.FullCheck.Click += new System.EventHandler(this.FullCheck_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 295);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Check For Updates";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Launcher
             // 
             this.AcceptButton = this.Play;
@@ -202,6 +213,7 @@ namespace ocstart
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(862, 423);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.FullCheck);
             this.Controls.Add(this.PatchInfo);
             this.Controls.Add(this.PathcProgress);
@@ -242,6 +254,7 @@ namespace ocstart
         private System.Windows.Forms.CheckBox SaveCred;
         private System.Windows.Forms.GroupBox LoginGroup;
         private System.Windows.Forms.Button FullCheck;
+        private System.Windows.Forms.Button button1;
     }
 }
 
