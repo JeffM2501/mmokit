34b7ca1b16c8be327e901f42c60dbdd5890820eb
JeffM2501 <JeffM2501@b8188ba6-572f-0410-9fd0-1f3cfbbf368d>
2009-03-17 16:30:15 +0000

    options dialogs

---
+++ mmokit/3dspeeders/3dSpeeders/AudioOptionsDialog.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _3dSpeeders
+{
+    public partial class AudioOptionsDialog : Form
+    {
+        ClientConfig config;
+        public AudioOptionsDialog( ClientConfig cfg )
+        {
+            config = cfg;
+            InitializeComponent();
+
+            EnableSound.Checked = config.enableSound;
+            VolumeVal.Text = config.volume.ToString();
+            VolumeBar.Value = config.volume;
+
+            enableItems();
+        }
+
+        private void Cancel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OK_Click(object sender, EventArgs e)
+        {
+            config.enableSound = EnableSound.Checked;
+            if (config.enableSound)
+                config.volume = VolumeBar.Value;
+        }
+
+        private void VolumeBar_Scroll(object sender, EventArgs e)
+        {
+            VolumeVal.Text = VolumeBar.Value.ToString();
+        }
+
+        void enableItems ()
+        {
+            VolumeBar.Enabled = EnableSound.Checked;
+            VolumeLabel.Enabled = EnableSound.Checked;
+            VolumeVal.Enabled = EnableSound.Checked;
+        }
+
+        private void EnableSound_CheckedChanged(object sender, EventArgs e)
+        {
+            enableItems();
+        }
+    }
+}
