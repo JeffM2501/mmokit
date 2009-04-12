44d8b39a5f9ce9dd4a38382c0a12b15ae110b3af
JeffM2501 <JeffM2501@b8188ba6-572f-0410-9fd0-1f3cfbbf368d>
2009-04-12 06:38:53 +0000

    math utility project that follows along the lines of the XNA bbox, sphere, frustum and plane classes, yet use OpenTKs math for those that you know, want openGL and Linux and stuff.. world project with an initial implementation of an octree system

---
+++ mmokit/3dspeeders/common/Math/BoundingFrustum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using OpenTK.Math;
+namespace Math3D
+{
+
+    public class BoundingFrustum
+    {
+         public const int CornerCount = 8;
+         Matrix4 matrix;
+
+         public Plane Near;
+         public Plane Far;
+         public Plane Left;
+         public Plane Right;
+         public Plane Top;
+         public Plane Bottom;
+
+         public BoundingFrustum(ref Matrix4 value)
+         {
+             matrix = value;
+         }
+
+         public void update (ref Matrix4 value)
+         {
+             matrix = value;
+         }
+    }
+}
---
+++ mmokit/3dspeeders/common/Math/Plane.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using OpenTK.Math;
+
+namespace Math3D
+{
+    public class Plane
+    {
+        public float D;
+        public Vector3 Normal;
+
+        public Plane(Vector3 norm, float d )
+        {
+            D = d;
+            Normal = norm;
+        }
+
+        public Plane (ref Vector3 p1, ref Vector3 p2, ref Vector3 p3 )
+        {
+            Vector3 v1 = p2 - p1;
+            Vector3 v2 = p3 - p1;
+            Normal = Vector3.Cross(v1, v2);
+        }
+
+        public Plane(float a, float b, float c, float d)
+        {
+
+        }
+
+        public PlaneIntersectionType Intersects(BoundingBox box)
+        {
+            return PlaneIntersectionType.Intersecting;
+        }
+
+        public PlaneIntersectionType Intersects(BoundingFrustum frustum)
+        {
+            return PlaneIntersectionType.Intersecting;
+
+        }
+
+        public PlaneIntersectionType Intersects(BoundingSphere sphere)
+        {
+            return PlaneIntersectionType.Intersecting;
+        }
+    }
+}
