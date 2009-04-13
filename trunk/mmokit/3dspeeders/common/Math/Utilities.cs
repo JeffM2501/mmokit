c0091b80f9c6d1ff0ad716ddd54292fe60e3f7a8
JeffM2501 <JeffM2501@b8188ba6-572f-0410-9fd0-1f3cfbbf368d>
2009-04-13 07:14:26 +0000

    Add rand functions that return floats

--- trunk/mmokit/3dspeeders/common/Math/Utilities.cs
+++ trunk/mmokit/3dspeeders/common/Math/Utilities.cs
@@ -28,4 +28,19 @@ namespace Math3D
             return angle * (180.0 / Math.PI); 
         }
     }
+
+    public class FloatRand
+    {
+        static Random rand = new Random();
+
+        public static float RandInRange(float min, float max)
+        {
+            return (float)(rand.NextDouble() * (max - min) + min);
+        }
+
+        public static float RandPlusMinus()
+        {
+            return RandInRange(-1, 1);
+        }
+    }
 }
