7ad29968b51ddfea5eadbebd29097d86475341ad
JeffM2501 <JeffM2501@b8188ba6-572f-0410-9fd0-1f3cfbbf368d>
2009-04-13 07:15:37 +0000

    implement an octree based world class

--- trunk/mmokit/3dspeeders/common/World/World.cs
+++ trunk/mmokit/3dspeeders/common/World/World.cs
@@ -7,8 +7,37 @@ using OpenTK.Math;
 
 namespace World
 {
-    class OctreeWorld : Octree
+    public class OctreeWorldObject : OctreeObject
     {
 
+    }
+
+    public class OctreeWorld : Octree
+    {
+        public List<OctreeWorldObject> objects = new List<OctreeWorldObject>();
+
+        public void Add(OctreeWorldObject item)
+        {
+            objects.Add(item);
+           // Add((OctreeObject)item);
+        }
+
+        public void BuildTree( BoundingBox initalBounds )
+        {
+            foreach (OctreeWorldObject item in objects)
+                ContainedObjects.Add(item as OctreeObject);
+
+            ContainerBox = new BoundingBox(initalBounds.Min,initalBounds.Max);
+            base.Distribute(0);
+        }
+
+        public void BuildTree()
+        {
+            foreach (OctreeWorldObject item in objects)
+                ContainedObjects.Add(item as OctreeObject);
+
+            Bounds();
+            base.Distribute(0);
+        }
     }
 }
