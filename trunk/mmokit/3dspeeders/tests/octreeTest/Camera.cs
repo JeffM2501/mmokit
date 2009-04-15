257ec166a45f854df06d42a5fca87dbfff8b9c89
JeffM2501 <JeffM2501@b8188ba6-572f-0410-9fd0-1f3cfbbf368d>
2009-04-15 07:16:36 +0000

    convert the camera to use GL lookat, instead of rotations. convert the camera over to keep it's own frustum "up to date" with the view draw what we know about the frustum that is snapped for culling.

--- trunk/mmokit/3dspeeders/tests/octreeTest/Camera.cs
+++ trunk/mmokit/3dspeeders/tests/octreeTest/Camera.cs
@@ -13,9 +13,43 @@ namespace Cameras
     public class Camera
     {
         Vector3 position = new Vector3();
+        public Vector3 EyePoint
+        {
+            get { return position; }
+        }
+
         float tilt = 0, spin = 0;
 
+        Vector3 up = new Vector3(0, 0, 1f);
         public bool ZIsUp = true;
+
+        float aspect = 1;
+        float fov = 45f;
+        public float FOV
+        {
+            get { return fov; }
+            set { fov = FOV; updatePerspective(); }
+        }
+
+        float hither = 1f;
+        public float NearPlane
+        {
+            get { return hither; }
+            set { hither = NearPlane; updatePerspective(); }
+        }
+
+        float yon = 1000.0f;
+        public float FarPlane
+        {
+            get { return yon; }
+            set { yon = FarPlane; updatePerspective(); }
+        }
+
+        BoundingFrustum frustum = new BoundingFrustum();
+        public BoundingFrustum ViewFrustum
+        {
+            get { return frustum; }
+        }
 
         public void move (Vector3 pos)
         {
@@ -60,16 +94,33 @@ namespace Cameras
             return forward;
         }
 
+        void updatePerspective ( )
+        {
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            Glu.Perspective((double)fov, (double)aspect, (double)hither, (double)yon);
+            frustum.updateProjection(Matrix4.Perspective(fov,aspect, hither, yon));
+        }
+
+        public void Resize(Int32 width, Int32 height)
+        {
+            aspect = width/(float)height;
+            updatePerspective();
+        }
+
         public void Execute()
         {
             GL.MatrixMode(MatrixMode.Modelview);
             GL.LoadIdentity();
 
-			GL.Rotate(tilt, 1.0f, 0.0f, 0.0f);			// pops us to the tilt
-            GL.Rotate(-spin+90.0f, 0.0f, 1.0f, 0.0f);			// gets us on our rot
-            GL.Translate(-position.X, -position.Z, position.Y);
+// 			GL.Rotate(tilt, 1.0f, 0.0f, 0.0f);			// pops us to the tilt
+//             GL.Rotate(-spin+90.0f, 0.0f, 1.0f, 0.0f);			// gets us on our rot
+//             GL.Translate(-position.X, -position.Z, position.Y);
+// 
+//             GL.Rotate(-90,1,0,0);
 
-            GL.Rotate(-90,1,0,0);
+            frustum.update(Matrix4.LookAt(position, position + Forward(), up));
+            Glu.LookAt(position, position + Forward(), up);
         }
     }
 }
