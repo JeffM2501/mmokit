f2a06ebb080de650c38b72d0da36d6324a246e75
JeffM2501 <JeffM2501@b8188ba6-572f-0410-9fd0-1f3cfbbf368d>
2009-04-17 05:10:15 +0000

    add plane point intersections add basic polygon class with clipper add BoundingCylinder

---
+++ mmokit/3dspeeders/common/Math/Polygon.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+using OpenTK.Math;
+
+namespace Math3D
+{
+    class Polygon
+    {
+        List<Vector3> points;
+        Plane plane;
+
+        public bool Valid ()
+        {
+            return points.Count > 2;
+        }
+
+        public bool Clip ( Plane clipPlane)
+        {
+            List<Vector3> newPoints = new List<Vector3>();
+
+            foreach (Vector3 p in points)
+            {
+                if (clipPlane.Intersects(p) != PlaneIntersectionType.Back)
+                    newPoints.Add(p);
+            }
+            points = newPoints;
+            return Valid();
+        }
+
+        public static Polygon Clip ( Polygon input, Plane clipPlane )
+        {
+            List<Vector3> newPoints = new List<Vector3>();
+
+            foreach(Vector3 p in input.points)
+            {
+                if (clipPlane.Intersects(p) != PlaneIntersectionType.Back)
+                    newPoints.Add(p);
+            }
+
+            Polygon outPoly = new Polygon();
+            outPoly.plane = new Plane(input.plane.Normal, input.plane.D);
+            outPoly.points = newPoints;
+
+            return outPoly;
+        }
+    }
+}
