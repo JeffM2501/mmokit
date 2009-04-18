6d3e52e5e3535eff3e569fb2ea1156e084cbf426
JeffM2501 <JeffM2501@b8188ba6-572f-0410-9fd0-1f3cfbbf368d>
2009-04-18 18:19:56 +0000

    basic utils (rel paths, etc..)

---
+++ trunk/mmokit/3dspeeders/common/Utilities/TextUtils.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Utilities.Text
+{
+    public class PathUtils
+    {
+
+        public static string MakePathRelitive(string rootpath, string outpath)
+        {
+            string[] rootChunks = Path.GetDirectoryName(rootpath).Split(Path.DirectorySeparatorChar.ToString().ToCharArray());
+            string[] outchunks = outpath.Split(Path.DirectorySeparatorChar.ToString().ToCharArray());
+
+            string relPath = string.Empty;
+
+            int i = 0;
+            for (i = 0; i < rootChunks.Length; i++)
+            {
+                if (i >= outchunks.Length)
+                    return outpath;
+
+                if (rootChunks[i] != outchunks[i])
+                {
+                    relPath += ".." + Path.DirectorySeparatorChar.ToString();
+                }
+            }
+
+            for (; i < outchunks.Length; i++)
+            {
+                relPath += outchunks[i];
+                if (i != outchunks.Length - 1)
+                    relPath += Path.DirectorySeparatorChar.ToString();
+            }
+            return relPath;
+        }
+    }
+}
