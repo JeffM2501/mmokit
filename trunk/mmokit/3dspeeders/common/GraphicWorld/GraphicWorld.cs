794368f76351aefe4779bedcdaed4d5a942f3b0e
JeffM2501 <JeffM2501@b8188ba6-572f-0410-9fd0-1f3cfbbf368d>
2009-04-19 17:55:49 +0000

    draw the walls with the GroundRenderer setup the world to reload cleanly (for editor use)

--- trunk/mmokit/3dspeeders/common/GraphicWorld/GraphicWorld.cs
+++ trunk/mmokit/3dspeeders/common/GraphicWorld/GraphicWorld.cs
@@ -2,30 +2,41 @@
 using System.Collections.Generic;
 using System.Drawing;
 
+using Drawables;
 using Drawables.Materials;
 using Drawables.Models;
+using Drawables.DisplayLists;
 using World;
-using Drawables;
 using Math3D;
 
 namespace GraphicWorlds
 {
     public class GraphicWorld
     {
-        public Dictionary<string, Material> materials;
-        public Dictionary<string, Model> models;
+        public Dictionary<string, Material> materials = null;
+        public Dictionary<string, Model> models = null;
         public ObjectWorld world = new ObjectWorld();
 
         GroundRenderer ground = new GroundRenderer();
+
+        public void Flush()
+        {
+            ground = new GroundRenderer();
+            world.Flush();
+            if (materials != null)
+                materials.Clear();
+            if (models != null)
+                models.Clear();
+        }
 
         public void AttatchMeshes ()
         {
             foreach( WorldObject o in world.objects )
             {
-                if (o.Tag == null && o.ObjectName != string.Empty)
+                if (o.tag == null && o.objectName != string.Empty)
                 {
-                    if (models.ContainsKey(o.ObjectName))
-                        o.Tag = models[o.ObjectName];
+                    if (models.ContainsKey(o.objectName))
+                        o.tag = models[o.objectName];
                 }
             }
         }
@@ -41,8 +52,13 @@ namespace GraphicWorlds
             // consolidate the materials in the system
             if (materials != null)
             {
-                foreach(KeyValuePair<string,Material> mat in materials)
-                    materials[mat.Key] = MaterialSystem.system.getMaterial(mat.Value);
+                Dictionary<string, Material> newMats = new Dictionary<string, Material>();
+                foreach (KeyValuePair<string, Material> mat in materials)
+                {
+                    mat.Value.Invalidate();
+                    newMats.Add(mat.Key, MaterialSystem.system.getMaterial(mat.Value));
+                }
+                materials = newMats;
             }
 
             ground.Setup(world);
--- trunk/mmokit/3dspeeders/common/GraphicWorld/GroundRenderer.cs
+++ trunk/mmokit/3dspeeders/common/GraphicWorld/GroundRenderer.cs
@@ -16,17 +16,27 @@ using World;
 
 namespace GraphicWorlds
 {
-    class GroundRenderer : ListableItem
+    class GroundRenderer
     {
+        DisplayList groundList = new DisplayList();
+        DisplayList wallList = new DisplayList();
+
         Material groundMaterial = null;
         Material wallMaterial = null;
 
         Vector3 size;
 
+        float wallHeight = 0;
+
         float uvScale = 1.0f;
 
         public void Setup(ObjectWorld world )
         {
+            groundList.Invalidate();
+            wallList.Invalidate();
+
+            size = world.size;
+            
             if (groundMaterial != null)
                 groundMaterial.Invalidate();
 
@@ -37,35 +47,124 @@ namespace GraphicWorlds
                 groundMaterial = MaterialSystem.system.getMaterial(new Material(Color.ForestGreen));
 
             if (world.groundUVSize > 0)
-                uvScale = world.groundUVSize;
+                uvScale = 1f/world.groundUVSize;
 
-            size = world.size;
+            DrawablesSystem.system.addItem(groundMaterial, new ExecuteCallback(DrawGround),DrawablesSystem.FirstPass, groundList);
 
-            DrawablesSystem.system.addItem(groundMaterial, new ExecuteCallback(DrawGround),DrawablesSystem.FirstPass);
+            wallHeight = world.wallHeight;
+            if (wallHeight > 0)
+            {
+                if (world.wallMaterialName != string.Empty)
+                    wallMaterial = MaterialSystem.system.getMaterial(world.wallMaterialName);
+                if (wallMaterial == null)
+                    wallMaterial = MaterialSystem.system.getMaterial(new Material(Color.Brown));
+
+                DrawablesSystem.system.addItem(wallMaterial, new ExecuteCallback(DrawGround), DrawablesSystem.FirstPass, wallList);
+            }
         }
 
-        protected override void GenerateList()
+        void BuildGroundList()
         {
+            groundList.Start();
             GL.Begin(BeginMode.Quads);
             GL.Normal3(0, 0, 1f);
 
-            GL.TexCoord2(size.X * uvScale, size.Y * uvScale);
+            GL.TexCoord2(size.X * uvScale, -size.Y * uvScale);
             GL.Vertex3(size.X, size.Y, 0);
 
-            GL.TexCoord2(-size.X * uvScale, size.Y * uvScale);
+            GL.TexCoord2(-size.X * uvScale, -size.Y * uvScale);
             GL.Vertex3(-size.X, size.Y, 0);
 
-            GL.TexCoord2(-size.X * uvScale, -size.Y * uvScale);
+            GL.TexCoord2(-size.X * uvScale, size.Y * uvScale);
             GL.Vertex3(-size.X, -size.Y, 0);
 
-            GL.TexCoord2(size.X * uvScale,- size.Y * uvScale);
+            GL.TexCoord2(size.X * uvScale,size.Y * uvScale);
             GL.Vertex3(size.X, -size.Y, 0);
             GL.End();
+            groundList.End();
+        }
+
+        void BuildWallList()
+        {
+            wallList.Start();
+
+            GL.Begin(BeginMode.Quads);
+
+            // y+
+            GL.Normal3(0, -1f, 0);
+            GL.TexCoord2(size.X * uvScale, 0);
+            GL.Vertex3(size.X, size.Y, 0);
+
+            GL.TexCoord2(size.X * uvScale, wallHeight * uvScale);
+            GL.Vertex3(size.X, size.Y, wallHeight);
+          
+            GL.TexCoord2(-size.X * uvScale, wallHeight * uvScale);
+            GL.Vertex3(-size.X, size.Y, wallHeight);
+
+            GL.TexCoord2(-size.X * uvScale, 0);
+            GL.Vertex3(-size.X, size.Y, 0);
+
+            // y-
+            GL.Normal3(0, 1f, 0);
+            GL.TexCoord2(-size.X * uvScale, 0);
+            GL.Vertex3(-size.X, -size.Y, 0);
+
+            GL.TexCoord2(-size.X * uvScale, wallHeight * uvScale);
+            GL.Vertex3(-size.X, -size.Y, wallHeight);
+
+            GL.TexCoord2(size.X * uvScale, wallHeight * uvScale);
+            GL.Vertex3(size.X, -size.Y, wallHeight);
+ 
+            GL.TexCoord2(size.X * uvScale, 0);
+            GL.Vertex3(size.X, -size.Y, 0);
+
+
+            // X+
+            GL.Normal3(-1f, 0, 0);
+            GL.TexCoord2(-size.Y * uvScale, 0);
+            GL.Vertex3(size.X, -size.Y, 0);
+
+            GL.TexCoord2(-size.Y * uvScale, wallHeight * uvScale);
+            GL.Vertex3(size.X, -size.Y, wallHeight);
+
+            GL.TexCoord2(size.Y * uvScale, wallHeight * uvScale);
+            GL.Vertex3(size.X, size.Y, wallHeight);
+          
+            GL.TexCoord2(size.Y * uvScale, 0);
+            GL.Vertex3(size.X, size.Y, 0);
+
+            // X-
+            GL.Normal3(1f, 0, 0);
+            GL.TexCoord2(size.Y * uvScale, 0);
+            GL.Vertex3(-size.X, size.Y, 0);
+
+            GL.TexCoord2(size.Y * uvScale, wallHeight * uvScale);
+            GL.Vertex3(-size.X, size.Y, wallHeight);
+
+            GL.TexCoord2(-size.Y * uvScale, wallHeight * uvScale);
+            GL.Vertex3(-size.X, -size.Y, wallHeight);
+
+            GL.TexCoord2(-size.Y * uvScale, 0);
+            GL.Vertex3(-size.X, -size.Y, 0);
+
+            GL.End();
+
+            wallList.End();
         }
 
         protected bool DrawGround(Material mat, object tag)
         {
-            base.Execute();
+            DisplayList list = tag as DisplayList;
+            if (list == null)
+                return false;
+
+            if (list == groundList && !groundList.Valid())
+                BuildGroundList();
+            else if (list == wallList && !wallList.Valid())
+                BuildWallList();
+
+            list.Call();
+
             return true;
         }
     }
