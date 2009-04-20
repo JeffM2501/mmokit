e75e32c74168ff1433921237cce3abc02a8b1f06
JeffM2501 <JeffM2501@b8188ba6-572f-0410-9fd0-1f3cfbbf368d>
2009-04-20 06:43:50 +0000

    start state and world classes for the server

---
+++ mmokit/3dspeeders/gameserver/GameWorld.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+using World;
+
+namespace gameserver
+{
+    public class GameWorld
+    {
+        public ObjectWorld world;
+        public FileInfo mapFile;
+
+        public bool loadWorldFile ( string file )
+        {
+            mapFile = new FileInfo(file);
+            if (!mapFile.Exists)
+                return false;
+
+            WorldFile worldFile = new WorldFile();
+            if (!WorldFile.read(out worldFile, mapFile))
+                return false;
+
+            // grab off the part we care about
+            world = worldFile.world;
+            world.BuildTree();
+
+            return true;
+        }
+    }
+}
