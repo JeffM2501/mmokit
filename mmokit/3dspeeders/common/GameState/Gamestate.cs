f28d152363b6346c0bc662c62f578a33d3d6627e
JeffM2501 <JeffM2501@b8188ba6-572f-0410-9fd0-1f3cfbbf368d>
2009-04-20 23:37:00 +0000

    start generic gamestate system

---
+++ mmokit/3dspeeders/common/GameState/Gamestate.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using World;
+using OpenTK.Math;
+
+namespace GameStates
+{
+    public delegate bool GameStateUpdateCallback(GameStateObject obj);
+
+    public class GameStateUpdateRecord
+    {
+        public Vector3 Position;
+        public Vector3 LinearVelocity;
+        public Vector3 Rotation;
+        public Vector3 RotaryVelocity;
+    }
+
+    public class GameStateObject
+    {
+        public object Tag;
+
+        public GameStateUpdateCallback UpdateCallback;
+
+        public GameStateUpdateRecord CurrentState;
+        public GameStateUpdateRecord LastState;
+
+        public double StartTime = 0;
+        public double LastUpdateTime = 0;
+        public double LifeTime = 0;
+
+        public GameStateObject (GameStateUpdateCallback cb, object tag, double now)
+        {
+            UpdateCallback = cb;
+            Tag = tag;
+            StartTime = now;
+            LastUpdateTime = now;
+        }
+
+        public void StateUpdate ( GameStateUpdateRecord state, double now )
+        {
+            LastState = state;
+            LastUpdateTime = now;
+        }
+
+        public void DRUpdate( double now )
+        {
+            LifeTime = now - StartTime;
+            UpdateCallback(this);
+        }
+    }
+
+    public class GameState
+    {
+        public ObjectWorld world;
+
+        public Dictionary<object, GameStateObject> StateObjects = new Dictionary<object, GameStateObject>();
+
+        public GameState (ObjectWorld w)
+        {
+            world = w;
+        }
+
+        public void Add ( object tag, GameStateObject obj )
+        {
+            if (tag == null || obj == null)
+                return;
+
+            StateObjects.Add(tag, obj);
+        }
+
+        public void Remove( object tag )
+        {
+            if (tag == null)
+                return;
+
+            if (StateObjects.ContainsKey(tag))
+                StateObjects.Remove(tag);
+        }
+    }
+}
